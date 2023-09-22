ef0df8e97e5061e48974b34e89c187660d59214f
MariGarcia91 <145747237+MariGarcia91@users.noreply.github.com>
2023-09-22 11:09:15 +0200

    Add files via upload

---
+++ Dia1.py
@@ -0,0 +1,66 @@
+#Qué bonito es Python
+print ("Hola Mundo")
+print ("Me llamo Mari")
+#Nuestra primera variable
+x = 5
+print("print(5):")
+print(5)            #Imprime 5
+print("print(5+5):")
+print(5+5)          #Imprime 10
+print("print(x):")
+print(x)            #Imprime 5
+print ("print(x+10):")
+print(x+10)         #Imprime 15
+
+#Creamos una variable con texto (string) "str" (("int"=números enteros))
+
+nombre = "Celia"
+numero = 8
+print(nombre)
+print("Sumo nombre+numero:")
+print('print("Celia"+"8")')
+print("Celia"+"8")
+print('print(nombre+str(numero)):')
+print(nombre+str(numero)) #convierto el 8 en string para que salga al lado del nombre.
+print("Escribo 10 veces CuCu") #Importante para pruebas técnicas.
+print("CuCu"*10)
+print() #forma puerca de salto de línea
+print("\nHacemos operaciones matemáticas") #Forma buena de hacerlo "\n", hace el salto de línea. (la barra le dice al string que no trabaje como trabaja normalmente)
+print("\nHacemos \t \"operaciones\" \t matemáticas\\")
+
+#\t sirve para tabular \" con comillas anula la funcionalidad de las comillas y las muestra por pantallas,
+#si ponemos barra delante de barra sale la barra.
+
+num1 = 2
+num2 = 8
+suma = num1+num2
+resta = num1-num2
+multp = num1*num2
+div = num1/num2
+print("Suma = " + str(suma) + "\
+| Resta = " + str(resta) + "\
+| Multiplicacion = " + str(multp) + "\
+| Division = " + str(div))
+
+#Float=números con coma flotante (numeros con decimales, numeros no enteros) también se les llama doble (dábol).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
